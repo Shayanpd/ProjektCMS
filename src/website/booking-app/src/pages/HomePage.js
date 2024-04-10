b52b0b2601c207e4e02b7246d78fcf7626318e21
DonolPaz <danielpazirai@gmail.com>
2024-04-10 19:19:53 +0200

    fixed cors error in .yml file, added code in homepage to request data (will be moved in later update)

--- src/website/booking-app/src/pages/HomePage.js
+++ src/website/booking-app/src/pages/HomePage.js
@@ -1,12 +1,49 @@
 // src/pages/HomePage.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import EventList from '../components/EventList';
 import eventImage from '../resources/events.png';
 
 const HomePage = () => {
-    console.log("HomePage is rendering");
+    console.log("HomePage is rendering"); 
+    
+    useEffect(() => {
+        // Create a authenticate-request to retrieve an access_token
+        fetch('http://localhost:8055/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: 'admin@example.com',
+                password: 'd1r3ctu5'
+            })
+        })
+        .then(res => res.json())
+        .then(authData => {
+            // Extract access_token from response
+            const accessToken = authData.data.access_token;
+    
+            // Use access_token for a GET-request
+            fetch('http://localhost:8055/items/test', {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+        })
+        .catch(error => {
+            console.error('Error authenticating:', error);
+        });
+    }, []);
+
     return (
         <div>
             <Header />
@@ -17,6 +54,7 @@ const HomePage = () => {
             <Footer />
         </div>
     );
+   
 };
 
 export default HomePage;
