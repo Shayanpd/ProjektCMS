675272e6c64caa5b37fea94d4e11fb2a3d80fe0e
DonolPaz <danielpazirai@gmail.com>
2024-04-16 20:42:09 +0200

    .env variables added, token in localStorage test

--- src/website/booking-app/src/pages/HomePage.js
+++ src/website/booking-app/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 // src/pages/HomePage.js
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import EventList from '../components/EventList';
@@ -10,50 +10,42 @@ const HomePage = () => {
     console.log("HomePage is rendering");
     const [loading, setLoading] = useState(false);
 
-
-    const sendRequestToCMS = () => {
+    useEffect(() => {
+        
+        fetch('http://localhost:8055/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: process.env.REACT_APP_EMAIL,
+            password: process.env.REACT_APP_PASSWORD
+        })
+        })
+        .then(res => res.json())
+        .then(authData => {
+            // Extract access_token from response
+            const accessToken = authData.data.access_token;
+            localStorage.setItem('access_token', accessToken);  
+        })
+    
+    }, []);
+    
+    const retrieveTestCollectionFromCMS = () => {
         setLoading(true); // Indicate the start of an API call
 
-
-        // Create an authenticate-request to retrieve an access_token
-        fetch('http://localhost:8055/auth/login', {
-            method: 'POST',
+        // Use access_token for a GET-request
+        fetch('http://localhost:8055/items/test', {
             headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: 'admin@example.com',
-                password: 'd1r3ctu5'
-            })
+                'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+            }
         })
-            .then(res => res.json())
-            .then(authData => {
-                // Extract access_token from response
-                const accessToken = authData.data.access_token;
-
-                // Use access_token for a GET-request
-                fetch('http://localhost:8055/items/test', {
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data);
-                        setLoading(false); // API call ended, remove loading indication
-                        alert('Request sent successfully'); // Show an alert on success
-                    })
-                    .catch(error => {
-                        console.error('Error fetching data:', error);
-                        setLoading(false); // API call ended, remove loading indication
-                        alert('Failed to send request.'); // Alert the user on error
-                    });
-            })
-            .catch(error => {
-                console.error('Error authenticating:', error);
-                setLoading(false); // API call ended, remove loading indication
-                alert('Authentication failed.'); // Alert the user on error
-            });
+        .then(res => res.json())
+        .then(responseData => {
+            console.log(responseData);
+            setLoading(false);
+        })
+        
     };
 
 
@@ -67,7 +59,7 @@ const HomePage = () => {
             <div className="fetch-events-button-container">
                 {/* Ensure the className here matches your new CSS for the button */}
                 <button
-                    onClick={sendRequestToCMS}
+                    onClick={() => retrieveTestCollectionFromCMS()}
                     disabled={loading}
                     className="fetch-events-button"
                 >
