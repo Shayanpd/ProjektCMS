bf927929c177ce6bfc6177d23bdaee7329a98c72
DonolPaz <danielpazirai@gmail.com>
2024-04-19 12:13:45 +0200

    access token small change

--- src/website/booking-app/src/pages/HomePage.js
+++ src/website/booking-app/src/pages/HomePage.js
@@ -5,35 +5,45 @@ import Footer from '../components/common/Footer';
 import EventList from '../components/EventList';
 import eventImage from '../resources/events.png';
 import './HomePage.css'; // Add this line to import styles
+import { createDirectus, graphql } from '@directus/sdk';
+
+interface test {
+    id: Number;
+    Name: string;
+    description: string;
+}
 
 const HomePage = () => {
     console.log("HomePage is rendering");
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+
+        if (!localStorage.getItem('access_token')) {
+            fetch('http://localhost:8055/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: process.env.REACT_APP_EMAIL,
+                    password: process.env.REACT_APP_PASSWORD
+                })
+            })
+            .then(res => res.json())
+            .then(authData => {
+                // Extract access_token from response and save in localStorage
+                const accessToken = authData.data.access_token;
+                localStorage.setItem('access_token', accessToken);
+                console.log('Updated token!')  
+            })
+        }
         
-        fetch('http://localhost:8055/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: process.env.REACT_APP_EMAIL,
-            password: process.env.REACT_APP_PASSWORD
-        })
-        })
-        .then(res => res.json())
-        .then(authData => {
-            // Extract access_token from response
-            const accessToken = authData.data.access_token;
-            localStorage.setItem('access_token', accessToken);  
-        })
-    
     }, []);
     
     const retrieveTestCollectionFromCMS = () => {
         setLoading(true); // Indicate the start of an API call
-
+        
         // Use access_token for a GET-request
         fetch('http://localhost:8055/items/test', {
             headers: {
