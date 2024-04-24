4c109aaf56681a9529b91c1ccdc424ba33f97ce8
Shayan <shayanpd@kth.se>
2024-04-24 18:54:56 +0200

    Eventdb test

---
+++ src/TestDatabaser/EventDatabas/create_table_ticket.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `TICKET` (
+    `ticketId` INT NOT NULL AUTO_INCREMENT,
+    `bookingId` INT NOT NULL,
+    `ticketCode` VARCHAR(255) NOT NULL,
+    `seat` VARCHAR(255),
+    PRIMARY KEY (`ticketId`),
+    FOREIGN KEY (`bookingId`) REFERENCES `BOOKING`(`bookingId`) ON DELETE CASCADE
+);
